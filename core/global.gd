67f7a8947956bfc3ca2c5d81676b698740536821
Peter Gilbert <petegilb@bu.edu>
2026-02-21 15:00:50 +0900

    fix invalid reference when testing dialogic timeline

--- core/global.gd
+++ core/global.gd
@@ -1,6 +1,6 @@
 extends Node
 
-@onready var main: Main = get_tree().get_nodes_in_group("main")[0]
+@onready var main: Main = get_tree().get_nodes_in_group("main")[0] if get_tree().get_nodes_in_group("main") else null
 
 func get_game_state() -> Main.GameState:
 	return main.state
